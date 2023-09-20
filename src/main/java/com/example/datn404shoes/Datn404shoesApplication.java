6b35bd22cc8201b3a11538146ed4dd5d574a6774
LucMinhQuan <quanluc2102@gmail.com>
2023-09-20 23:31:10 +0700

    create new project

---
+++ src/main/java/com/example/datn404shoes/Datn404shoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.datn404shoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datn404shoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Datn404shoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datn404shoes/Datn404shoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datn404shoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Datn404shoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
