099fc5d8d29ab6cb263e4fdcb9c6e12ca1b011c8
LucMinhQuan <quanluc2102@gmail.com>
2023-11-22 22:15:42 +0700

    update thống kê

--- frontend/src/components/thongkecomponent/ThongKeDoanhThuSanPham.jsx
+++ frontend/src/components/thongkecomponent/ThongKeDoanhThuSanPham.jsx
@@ -15,6 +15,7 @@ class ThongKeDoanhThuSanPham extends Component {
         const currentYear = new Date().getFullYear();
         this.state = {
             thongKeSanPham: [],
+            thongKeThangNew: [],
             startDate: '',
             endDate: '',
             pageCount: 0,
@@ -41,6 +42,7 @@ class ThongKeDoanhThuSanPham extends Component {
 
     componentDidMount() {
         this.fetchData();
+        this.fetchDataThongKeThangNew();
         this.handleFilterChange('Theo ngày'); // Thêm dòng này
         this.fetchDoanhThuNgay();
         this.fetchDoanhThuTuan();
@@ -101,6 +103,25 @@ class ThongKeDoanhThuSanPham extends Component {
     };
 
 
+    fetchDataThongKeThangNew = (page = 1) => {
+        const { startDate, endDate, thongKeType, selectedMonth,selectedYear} = this.state;
+        console.log(selectedMonth)
+        console.log(selectedYear)
+         if (thongKeType === 'nam') {
+            thongkeservice.getDoanhThuThangNew(selectedYear, page)
+                .then(data => {
+                    this.setState({thongKeThangNew: data}, () => {
+                        this.renderCombinedChartThongKeDoanhThuNew();
+                    });
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        } else {
+
+        }
+
+    };
 
 
     fetchDoanhThuNgay = () => {
@@ -224,12 +245,17 @@ class ThongKeDoanhThuSanPham extends Component {
         this.setState({totalRevenue});
     };
 
-    handleInputChange = (event) => {
+     handleInputChange = (event) => {
         this.setState({[event.target.name]: event.target.value});
     };
 
     handleThongKeClick = () => {
         this.fetchData();
+        this.setState({showTable: false});
+    };
+
+    handleThongKeThangNewClick = () => {
+        this.fetchDataThongKeThangNew();
         this.setState({showTable: false});
     };
 
@@ -352,6 +378,101 @@ class ThongKeDoanhThuSanPham extends Component {
                 title: {
                     display: true,
                     text: 'Biểu đồ kết hợp - Số lượng đã bán và Tổng tiền',
+                    font: {
+                        size: 16,
+                    },
+                },
+            },
+        };
+
+        const ctx = this.combinedChartRef.current?.getContext('2d');
+
+        if (this.combinedChart) {
+            this.combinedChart.destroy();
+        }
+
+        if (ctx) {
+            this.combinedChart = new Chart(ctx, {
+                type: 'bar',
+                data: chartData,
+                options: chartOptions,
+            });
+        }
+    }
+
+    renderCombinedChartThongKeDoanhThuNew() {
+        const chartData = {
+            labels: this.state.thongKeThangNew.map(th => th[0]),
+            datasets: [
+                {
+                    label: 'Doanh thu',
+                    type: 'bar',
+                    data: this.state.thongKeThangNew.map(th => th[2]),
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 3,
+                    yAxisID: 'y-axis-total',
+                    order: 1,
+                },
+                {
+                    label: 'Số lượng đã bán',
+                    data: this.state.thongKeThangNew.map(th => th[1]),
+                    borderColor: 'rgba(75, 192, 192, 2)',
+                    borderWidth: 4,
+                    fill: true,
+                    yAxisID: 'y-axis-quantity',
+                    order: 1,
+                },
+            ],
+        };
+
+        const chartOptions = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-quantity',
+                        type: 'linear',
+                        position: 'right',
+                        grid: {
+                            drawOnChartArea: true,
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1,
+                            callback: (value) => value + ' units',
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Số lượng',
+                            font: {
+                                size: 14,
+                            },
+                            position: 'top',
+                        },
+                    },
+                    {
+                        id: 'y-axis-total',
+                        type: 'linear',
+                        position: 'left',
+                        ticks: {
+                            beginAtZero: true,
+                            callback: (value) => '$' + value,
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Doanh thu',
+                            font: {
+                                size: 14,
+                            },
+                            position: 'top',
+                        },
+                    },
+                ],
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Biểu đồ doanh thu theo tháng',
                     font: {
                         size: 16,
                     },
@@ -714,8 +835,8 @@ class ThongKeDoanhThuSanPham extends Component {
                 <section className="section dashboard">
                     <Tabs>
                         <TabList>
-                            <Tab>Biểu đồ thống kê</Tab>
-                            <Tab>Bảng thống kê</Tab>
+                            <Tab>Thống kê theo sản phẩm đã bán</Tab>
+                            <Tab>Thống kê doanh thu</Tab>
                             <Tab>Thống kê doanh thu</Tab>
                         </TabList>
 
@@ -869,9 +990,121 @@ class ThongKeDoanhThuSanPham extends Component {
 
                         {/* Add new TabPanel for "Thống kê doanh thu" */}
                         <TabPanel>
-                            {/* You can add content for the new tab here */}
-                            <h2>Thống kê doanh thu</h2>
-                            {/* ... */}
+                            <div className="thongke-controls">
+                                {/*<label>Chọn kiểu thống kê:</label>*/}
+                                {/*<select*/}
+                                {/*    name="thongKeType"*/}
+                                {/*    value={this.state.thongKeType}*/}
+                                {/*    onChange={this.handleThongKeTypeChange}*/}
+                                {/*>*/}
+                                {/*    <option value="ngay">Theo ngày</option>*/}
+                                {/*    <option value="thang">Theo tháng</option>*/}
+                                {/*    <option value="nam">Theo năm</option>*/}
+                                {/*</select>*/}
+
+
+                                {this.state.thongKeType === 'nam' && (
+                                    <>
+                                        <label>Chọn năm:</label>
+                                        <select
+                                            name="selectedYear"
+                                            value={this.state.selectedYear}
+                                            onChange={this.handleInputChange}
+                                        >
+                                            {/* Tạo các option từ năm 1990 đến 10 năm sau tính từ thời điểm hiện tại */}
+                                            {Array.from({ length: new Date().getFullYear() - 1990 + 11 }, (_, index) => {
+                                                const year = 1990 + index;
+                                                return (
+                                                    <option key={year} value={year}>
+                                                        {year}
+                                                    </option>
+                                                );
+                                            })}
+                                        </select>
+                                    </>
+                                )}
+
+
+                                <button onClick={this.handleThongKeThangNewClick}>Thống kê</button>
+                            </div>
+
+                            <div className="card combined-chart overflow-auto">
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        Biểu đồ thống kê doanh thu <span>| </span>
+                                    </h5>
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <canvas id="combinedChart" ref={this.combinedChartRef} width="400"
+                                                    height="200"></canvas>
+                                        </div>
+                                    </div>
+                                    <button className="btn btn-outline-secondary" onClick={this.handleToggleTable}>
+                                        {this.state.showTable ? (
+                                            <i className="bi bi-eye-slash"></i>
+                                        ) : (
+                                            <i className="bi bi-eye"></i>
+                                        )}
+                                    </button>
+                                </div>
+                            </div>
+
+                            {this.state.showTable && (
+                                <div className="card">
+                                    <div className="card-body">
+                                        <div className="d-flex justify-content-between align-items-center mb-3">
+                                            <h5 className="card-title">Bảng thống kê theo sản phẩm <span>|</span></h5>
+                                            <div>
+                                                <button onClick={this.handleExportToExcel}
+                                                        disabled={this.state.exportingToExcel}>
+                                                    {this.state.exportingToExcel ? 'Exporting...' : <FaFileExcel/>}
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div className="table-responsive">
+                                            <table className="table table-borderless datatable">
+                                                <thead>
+                                                <tr>
+                                                    <th>STT</th>
+                                                    <th>Sản phẩm</th>
+                                                    <th>Số lượng đã bán</th>
+                                                    <th>Tổng tiền</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                {this.state.thongKeThangNew.map((th, index) => (
+                                                    <tr key={index}>
+                                                        <td>{index + 1}</td>
+                                                        <td>{th[0]}</td>
+                                                        <td>{th[1]}</td>
+                                                        <td>{this.formatCurrency(th[2])} VND</td>
+                                                    </tr>
+                                                ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <ReactPaginate
+                                            previousLabel={"<"}
+                                            nextLabel={">"}
+                                            breakLabel={"..."}
+                                            breakClassName={"page-item"}
+                                            breakLinkClassName={"page-link"}
+                                            pageClassName={"page-item"}
+                                            pageLinkClassName={"page-link"}
+                                            previousClassName={"page-item"}
+                                            previousLinkClassName={"page-link"}
+                                            nextClassName={"page-item"}
+                                            nextLinkClassName={"page-link"}
+                                            pageCount={this.state.pageCount}
+                                            marginPagesDisplayed={2}
+                                            pageRangeDisplayed={5}
+                                            onPageChange={this.handlePageClick}
+                                            containerClassName={"pagination justify-content-center"}
+                                            activeClassName={"active"}
+                                        />
+                                    </div>
+                                </div>
+                            )}
                         </TabPanel>
                     </Tabs>
                 </section>
