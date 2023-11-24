c7a0afca4140731c2348258065b751bc4fa10753
TranDang-03 <danghansamu@gmail.com>
2023-11-24 17:13:08 +0700

    update ban hang offline

--- frontend/src/components/banhangoffline/banhang.jsx
+++ frontend/src/components/banhangoffline/banhang.jsx
@@ -43,6 +43,7 @@ class BanHangOffline extends Component {
 
         this.nextTabIndex = 0
     }
+
     componentDidMount() {
         BanHangService.getSPCT().then((res) => {
             this.setState({ sanPhamChiTiet: res.data })
@@ -50,6 +51,65 @@ class BanHangOffline extends Component {
             console.error("Error fetching data:", error);
         });
     }
+
+    getTotalQuantity = () => {
+        const { selectedProducts } = this.state;
+        return selectedProducts.reduce((total, product) => total + product.quantity, 0);
+    };
+
+    getTotalAmount = () => {
+        const { selectedProducts } = this.state;
+        return selectedProducts.reduce((total, product) => total + product.donGia * product.quantity, 0);
+    };
+
+    handleProductClick = (sanPhamChiTiet) => {
+        const { selectedProducts, indexProduct } = this.state;
+
+        this.setState({
+            indexProduct: indexProduct + 1,
+        }, () => {
+            const totalQuantity = this.getTotalQuantity();
+            console.log('Tổng số sản phẩm:', totalQuantity);
+
+            const totalAmount = this.getTotalAmount();
+            console.log('Tổng số tiền:', totalAmount);
+        });
+    };
+
+    addToCurrentTab = (product, tabKey) => {
+        const { tabList } = this.state;
+        console.log(product, tabKey)
+
+        const updatedTabList = tabList.map(tab => {
+            if (tab.key === tabKey) {
+                const updatedProducts = tab.products ? [...tab.products, product] : [product];
+                return {
+                    ...tab,
+                    products: updatedProducts
+                };
+            }
+            return tab;
+        });
+
+        this.setState({ tabList: updatedTabList });
+        console.log(tabList)
+    };
+
+    handleProductClick = (productId) => {
+        const { selectedProducts } = this.state;
+        const selectedProduct = selectedProducts.find(item => item.ma === productId.ma);
+
+        if (selectedProduct) {
+            const updatedSelectedProducts = selectedProducts.map(item =>
+                item.ma === productId.ma ? { ...item, quantity: item.quantity + 1 } : item
+            );
+            this.setState({ selectedProducts: updatedSelectedProducts });
+        } else {
+            const newSelectedProduct = { ...productId, quantity: 1 };
+            const updatedSelectedProducts = [...selectedProducts, newSelectedProduct];
+            this.setState({ selectedProducts: updatedSelectedProducts });
+        }
+    };
 
     onEdit = (tabKey, action) => {
         if (action === 'add' && this.state.tabList.length < 5) {
@@ -76,13 +136,10 @@ class BanHangOffline extends Component {
         }
     };
 
-    onDelete = tabKey => {
+    onDelete = (productId) => {
         this.setState(prevState => {
-            const newTabList = prevState.tabList.filter(tab => tab.key !== tabKey);
-            if (newTabList.length === 0) {
-                this.nextTabIndex = 1;
-            }
-            return { tabList: newTabList };
+            const updatedSelectedProducts = prevState.selectedProducts.filter(product => product.ma !== productId);
+            return { selectedProducts: updatedSelectedProducts };
         });
     };
 
@@ -129,23 +186,25 @@ class BanHangOffline extends Component {
                                             </Col>
 
 
-                                            <Col style={{ backgroundColor: '#fff', height: '75px', padding: '10px', display: 'flex', alignItems: 'center' }}>
-                                                <Col span={1} style={{ fontWeight: 'bold', fontSize: '15px' }} >1</Col>
-                                                <Col span={3} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} ><img style={{ height: '60px', width: '60px' }} src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/dda507d6073c4f44abb5d314d617250e_9366/Ultra_4DFWD_Running_Shoes_Grey_ID1686_HM1.jpg" /></Col>
-                                                <Col span={3} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} >  </Col>
-                                                <Col span={5} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} >  </Col>
-                                                <Col span={2} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} > <input type="number" className="soLuong" min="1" style={{ width: '50px' }} /></Col>
-                                                <Col span={4} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} > VND</Col>
-                                                <Col span={5} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} > VND</Col>
-                                                <Col span={1} style={{ transition: 'color 0.3s' }}>
-                                                    <DeleteOutlined
-                                                        onClick={() => this.onDelete(tabinfo.key)}
-                                                        style={{ cursor: 'pointer' }}
-                                                        onMouseEnter={(e) => e.target.style.color = 'red'}
-                                                        onMouseLeave={(e) => e.target.style.color = 'black'}
-                                                    />
+                                            {this.state.selectedProducts.map((product, index) => (
+                                                <Col style={{ backgroundColor: '#fff', height: '75px', padding: '10px', display: 'flex', alignItems: 'center' }}>
+                                                    <Col span={1} style={{ fontWeight: 'bold', fontSize: '15px' }} >{index + 1}</Col>
+                                                    <Col span={3} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} ><img style={{ height: '60px', width: '60px' }} src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/dda507d6073c4f44abb5d314d617250e_9366/Ultra_4DFWD_Running_Shoes_Grey_ID1686_HM1.jpg" /></Col>
+                                                    <Col span={3} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} > {product.ma} </Col>
+                                                    <Col span={5} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} > {product.sanPham.ten} </Col>
+                                                    <Col span={2} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} > <input type="number" className="soLuong" min="1" style={{ width: '50px' }} value={product.quantity} /></Col>
+                                                    <Col span={4} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} >{product.donGia} VND</Col>
+                                                    <Col span={5} style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }} > {product.donGia * product.quantity}VND</Col>
+                                                    <Col span={1} style={{ transition: 'color 0.3s' }}>
+                                                        <DeleteOutlined
+                                                            onClick={() => this.onDelete(product.ma)}
+                                                            style={{ cursor: 'pointer' }}
+                                                            onMouseEnter={(e) => e.target.style.color = 'red'}
+                                                            onMouseLeave={(e) => e.target.style.color = 'black'}
+                                                        />
+                                                    </Col>
                                                 </Col>
-                                            </Col>
+                                            ))}
 
                                         </div>
                                     </Tabs.TabPane>
@@ -167,7 +226,7 @@ class BanHangOffline extends Component {
                                         label: (
                                             <div style={{ overflowX: 'auto', overflowY: 'auto' }}>
                                                 <div>{index + 1}</div>
-                                                <img style={{ height: '60px', width: '60px', float: 'left' }} src={option.sanPham.anh} />
+                                                <img style={{ height: '60px', width: '60px', float: 'left' }} src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/dda507d6073c4f44abb5d314d617250e_9366/Ultra_4DFWD_Running_Shoes_Grey_ID1686_HM1.jpg" />  {/** src={option.sanPham.anh} */}
                                                 <div style={{ marginLeft: '75px' }}>{option.sanPham.ten} <br /> {'Size: '}{option.kichThuoc.giaTri} - {'Màu: '}{option.mauSac.ten}</div>
                                                 <div style={{ marginLeft: '75px' }}> {'Giá: '}{option.donGia}{' VND'} - SL: {option.soLuong}</div>
                                             </div>
@@ -176,15 +235,15 @@ class BanHangOffline extends Component {
                                     }))}
 
                                 />
-                                <Button style={{color: 'black', backgroundColor: '#fff'}}>Thêm</Button>
-                                
-                                <Button style={{float: 'right', marginRight: '20px', color: 'black', backgroundColor: '#fff'}}>Quét QR</Button>
+                                <Button style={{ color: 'black', backgroundColor: '#fff' }}>Thêm</Button>
 
-                                </div>
+                                <Button style={{ float: 'right', marginRight: '20px', color: 'black', backgroundColor: '#fff' }}>Quét QR</Button>
+
+                            </div>
                             <Flex wrap="wrap">
                                 {this.state.sanPhamChiTiet && this.state.sanPhamChiTiet.map((sanPhamChiTiet, index) => {
                                     return (
-                                        <Flex key={index} style={{ width: '50%', overflowX: 'auto', overflowY: 'auto', cursor: 'pointer' }} flex={'row'}>
+                                        <Flex onClick={() => this.handleProductClick(sanPhamChiTiet)} key={index} style={{ width: '50%', overflowX: 'auto', overflowY: 'auto', cursor: 'pointer' }} flex={'row'}>
                                             <div style={{ overflowX: 'auto', overflowY: 'auto' }} className="container_sell">
                                                 <div> <img style={{ height: '60px', width: '60px', float: 'left' }} src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/dda507d6073c4f44abb5d314d617250e_9366/Ultra_4DFWD_Running_Shoes_Grey_ID1686_HM1.jpg" /></div>
                                                 <div style={{ marginLeft: '75px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{sanPhamChiTiet.sanPham.ten}</div>
@@ -221,13 +280,13 @@ class BanHangOffline extends Component {
                             <div className="payment_sell">
                                 <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
                                     <Col>
-                                        <Col style={{ fontSize: '16px', margin: '5px 0px 5px 0px' }}>Tổng tiền: (0 sản phẩm)</Col>
+                                        <Col style={{ fontSize: '16px', margin: '5px 0px 5px 0px' }}>Tổng tiền: ({this.getTotalQuantity()} sản phẩm)</Col>
                                         <Col style={{ fontSize: '16px' }}>Mã khuyến mãi: </Col>
                                         <Col style={{ fontSize: '16px', marginTop: '5px ' }}>Giảm giá:</Col>
                                     </Col>
                                     <Col style={{ width: '55%', borderStyle: 'solid', borderTop: 'none', borderRight: 'none', borderLeft: 'none', borderWidth: '1px' }}>
-                                        <Col style={{ fontSize: '16px', textAlign: 'right', margin: '5px 0px 5px 0px' }}><span style={{ color: 'red' }}>222.500</span></Col>
-                                        <Col style={{ fontSize: '16px', textAlign: 'right' }}><Input type="text" placeholder="Nhập mã..." style={{ width: '120px', float: 'left' }} /> <Button style={{width: '75px', color: '#000', backgroundColor: 'rgb(255,255,255)'}}>Áp dụng</Button></Col>
+                                        <Col style={{ fontSize: '16px', textAlign: 'right', margin: '5px 0px 5px 0px' }}><span style={{ color: 'red' }}>{this.getTotalAmount()}</span></Col>
+                                        <Col style={{ fontSize: '16px', textAlign: 'right' }}><Input type="text" placeholder="Nhập mã..." style={{ width: '120px', float: 'left' }} /> <Button style={{ maxWidth: '75px', textAlign: 'center' }}>Áp dụng</Button></Col>
                                         <Col style={{ fontSize: '16px', textAlign: 'right', marginTop: '5px' }}>0</Col>
                                     </Col>
                                 </div>
@@ -237,7 +296,7 @@ class BanHangOffline extends Component {
                                         <Col style={{ fontWeight: 'bold', fontSize: '16px' }}>Tiền khách đưa</Col>
                                     </Col>
                                     <Col style={{ borderStyle: 'solid', borderTop: 'none', borderRight: 'none', borderLeft: 'none', borderWidth: '1px', paddingBottom: '10px' }}>
-                                        <Col style={{ fontWeight: 'bold', fontSize: '16px', textAlign: 'right', margin: '5px 0px 5px 0px' }}><span style={{ color: 'red' }}>222.500</span></Col>
+                                        <Col style={{ fontWeight: 'bold', fontSize: '16px', textAlign: 'right', margin: '5px 0px 5px 0px' }}><span style={{ color: 'red' }}> {Math.max(0, this.getTotalAmount() - 0)}</span></Col>
                                         <Col style={{ fontWeight: 'bold', fontSize: '16px', textAlign: 'right' }}><InputNumber min={1} onChange={this.onChangePay} style={{ border: 'none', fontSize: '19px', width: '210px' }} /></Col>
                                     </Col>
                                 </div>
@@ -251,7 +310,7 @@ class BanHangOffline extends Component {
                             </Flex>
                             <Flex flex={"row"} align="center" justify="space-between">
                                 <p style={{ fontSize: '16px', fontWeight: 'bold' }}>Tiền thừa trả khách</p>
-                                <p style={{ fontSize: '20px', fontWeight: 'bold' }}><span style={{color: 'red'}}>250.000</span></p>
+                                <p style={{ fontSize: '20px', fontWeight: 'bold' }}><span style={{ color: 'red' }}>250.000</span></p>
                             </Flex>
                         </div>
                         <div>
