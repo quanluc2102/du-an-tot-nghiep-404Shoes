e95d14150ebce698e9ec7311c5f40d25f4641a57
giangto2003 <giangtotdhg2810@gmail.com>
2024-01-17 19:20:18 +0700

    update NV, KH

---
+++ src/main/java/com/example/datn404shoes/service/serviceimpl/TaiKhoanVaThongTinServiceimpl.java
@@ -0,0 +1,13 @@
+package com.example.datn404shoes.service.serviceimpl;
+
+import com.example.datn404shoes.custom.TaiKhoanVaThongTin;
+import com.example.datn404shoes.service.TaiKhoanVaThongTinService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaiKhoanVaThongTinServiceimpl implements TaiKhoanVaThongTinService {
+    @Override
+    public TaiKhoanVaThongTin getOneByEmail(String email) {
+        return null;
+    }
+}
